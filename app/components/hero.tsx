c1eede7663a333322dc0f24fb32a5cc49ddd977e
AmritHehe <hopesalive1506@gmail.com>
2025-03-30 03:29:11 +0530

    major changes in the front page
    learned how to use fonts and yes major changes in the front page and very little changes in the bento grid

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -7,6 +7,52 @@ import { Spotlight } from "./ui/spotlight";
 import { TextGenerateEffect } from "./ui/text-generate-effect";
 import * as motion from "motion/react-client"
 //import { anticipate, easeIn, hover } from "motion";
+import Image from "next/image";
+
+import { Inter,  Tangerine ,Bangers ,Cinzel_Decorative , Cormorant , Young_Serif, Exo} from 'next/font/google'
+import { px } from "motion/react";
+ 
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: "--font-inter"
+})
+export const tangerine = Tangerine({
+    subsets: ['latin'],
+    display: 'swap',
+    variable: "--font-tangerine",
+    weight : '400'
+  })
+export const cinzel = Cinzel_Decorative({
+  weight : "700",
+  subsets: ['latin'],
+  display: 'swap',
+  variable: "--font-cinzel"
+})
+export const banger = Bangers({
+  subsets :['latin'], 
+  weight : '400',
+  display : 'swap',
+  variable :"--font-banger"
+}) 
+export const cormorant = Cormorant({
+   
+  weight : '500',
+  display : 'swap',
+  subsets : ['latin'],
+  style: ["normal", "italic"],
+  variable :"--font-cormorant",
+
+})
+
+export const ysf = Young_Serif({
+  weight:"400",
+  style : "normal", 
+  display : "swap", 
+  subsets : ["latin"],
+  variable: "--font-young"
+}) 
+
 
 
 export function Hero( ){ 
@@ -18,31 +64,73 @@ export function Hero( ){
     {/* <Boxes/> */}
     
     
-    
-    <div className="flex flex-col items-center  sm:w-5/6 z-20">
-    <Spotlight className="top-0  md:top-40 h-screen " fill='white'/>
-    <Spotlight className="scale-x-[-1] top-0  md:top-40 h-screen" />
-        <p className="tracking-widest font-normal">SEO-optimized? Check. Responsive? Double check. Hooked? You bet!</p>
-        <h1 className="text-5xl my-24 sm:text-6xl md:text-7xl font-bold text-center m-8 font-shrikhand inline">
+    <div  className="absolute inset-0 w-full "  >
+    {/* <div className="absolute bottom-0 left-0 w-[450px] h-[450px] bg-[rgb(255,180,220)] blur-[160px] opacity-60 rounded-full"></div>
+ 
+    <div className="absolute top-0 right-0 w-[450px] h-[450px] bg-[rgb(130,100,255)] blur-[160px] opacity-60 rounded-full"></div> */}
+     <div className="absolute bottom-0 left-0 w-[60vw] h-[60vw] max-w-[500px] max-h-[500px] bg-[rgb(255,180,220)] blur-[20vw] 2xl:blur-[10vw] opacity-80 rounded-full"></div>
+      {/* <svg>
+        <filter id="grainy">
+          <feTurbulence 
+              type="turbulance"
+              baseFrequency="0.65"
+            />
+
+        </filter>
+      </svg> */}
+      
+
+      <div className="absolute top-0 right-0 w-[60vw] h-[60vw] max-w-[500px] max-h-[500px] bg-[rgb(130,100,255)] blur-[20vw] 2xl:blur-[10vw]   opacity-80 rounded-full"></div>
+      
+      {/* <Image 
+         className="object-cover absolute inset-0 blur-sm"
+         src= "/D5.png"
+         alt="" 
+         fill
+         style={{objectFit:"cover" , position:"absolute" , inset:0 ,opacity : 0.5
+         }}
+        // priority
+      /> */}
+    </div>
+    <div className={`${cormorant.variable} flex flex-col items-center  sm:w-5/6 z-20`}>
+      <Spotlight className="top-0  md:top-40 h-screen " fill='black'/>
+      {/* <Spotlight className="scale-x-[-1] top-0  md:top-40 h-screen" />
+       */}
+        {/* <p className="tracking-widest font-normal">SEO-optimized? Check. Responsive? Double check. Hooked? You bet!</p> */}
+        {/* <h1 className="text-5xl  my-24 font-cormorant sm:text-6xl md:text-7xl  text-center m-8  inline">
         Serving up websites that feels like <p className="inline text-purple-300 ">  home</p>
+        </h1> */
+        }
+        <div  className="w-full flex  h-full justify-center scale-150 drop-shadow-[0px_0px_3px_rgba(16,39,108,0.8)] sm:scale-200 md:scale-250 lg:scale-400 my-48 p-24 items-center size-5">
+        <h1 className=  {`${tangerine.variable} text-8xl inline font-tangerine`}>P  
+          <h1 className={`${cormorant.variable} text-6xl font-cormorant inline-block`}>or</h1>
+          <h1 className={`${tangerine.variable} text-7xl mr-1 font-tangerline inline-block`}>t</h1>
+          <h1 className={`${cormorant.variable} text-6xl font-cormorant inline-block`}>foli</h1>
+          <h1 className={`${ysf.variable} text-5xl font-young font-thin inline-block`}>o</h1>
         </h1>
+        <br />
+        </div>
+        {/* <div>Amrit </div> */}
         {/* <h2 >
             Hi! I am  Amrit , A Mern Stack Dev based in Delhi
         </h2> */}
-        <TextGenerateEffect className="text-zinc-300 text-2xl" words ="Hi! I am  Amrit , A Mern Stack Dev based in Delhi" />
+        <TextGenerateEffect className={`${cormorant.variable}  font-cormorant italic`} words ="Hi! I am  Amrit , A Mern Stack Dev based in Delhi" />
         <motion.div drag dragConstraints={{ 
             left : 0 , 
             right : 0, 
             top : 0 , 
             bottom : 0
-        }}  whileHover = {{scale : 1.2}} className="bg-gradient-to-r from-slate-700 to-gray-800 p-2 rounded-2xl border-2 border-zinc-700 px-6 py-4 mt-10 ab"> <p className="inline-block">See My Work </p> <motion.div  whileHover={{rotate : 120}}  drag dragConstraints={{ 
+        }}  whileHover = {{scale : 1.2}} className=" bg-gradient-to-r from-gray-800 to-cyan-700 p-2 rounded-2xl border-2 border-zinc-700 px-6 py-4 mt-10 ab"> <p className="inline-block">See My Work </p> <motion.div  whileHover={{rotate : 120}}  drag dragConstraints={{ 
             left : 0 , 
             right : 0, 
             top : 0 , 
             bottom : 0
         }}  className=" px-1 inline-block"><Arrow/></motion.div>  </motion.div>
+        
+        
          
     </div>
+    
     {/* <Boxes className="opacity-25"/> */}
     </>
 }
--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
       <>
       <div className="overflow-x-hidden">
-        <main className="relative bg-gray-950 flex flex-col items-center justify-start   mx-auto h-screen  w-full sm:px:10 px-5">
+        <main className="relative bg-black flex flex-col items-center justify-start   mx-auto h-screen  w-full sm:px:10 px-5">
           <Navbar/>
 
             <div className=" max-w-7xl flex item-center justify-center m-25 w-full">
@@ -17,7 +17,7 @@ export default function Home() {
 
             </div>
         </main>
-        <div className="h-100dvh w-screen flex  justify-center items-center  ">
+        <div className="h-100dvh w-screen flex bg-black  justify-center items-center  ">
           <BentoGrid/>
             
          </div>
