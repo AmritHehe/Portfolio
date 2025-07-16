4e35230d396183794e7bd0da72a32ffe058a7db9
AmritHehe <hopesalive1506@gmail.com>
2025-07-16 22:17:35 +0530

    reverting changes as ios fix doesnt work

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 //import Image from "next/image";
 import Hero from "./components/hero";
 // import Navbar from "./components/Navbar";
@@ -8,20 +6,8 @@ import Hero from "./components/hero";
 import Introduction from "./components/Introduction";
 import Projects from "./components/projectspage";
 import { Connect } from "./components/connect";
-import { useEffect } from "react";
 
 export default function Home() {
-  // In your _app.js or a top-level useEffect
-  useEffect(() => {
-    const setVh = () => {
-      document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
-    };
-
-    setVh();
-    window.addEventListener('resize', setVh);
-    return () => window.removeEventListener('resize', setVh);
-  }, []);
-
   return (
       <>
       <div className="overflow-x-hidden">
